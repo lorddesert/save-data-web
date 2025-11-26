caa8dfd305321f99b62b4d4561cfafa9cd82ce12
lorddesert <sacha.nocetto@hotmail.com>
2025-11-26 12:12:12 -0300

    fix manifest is not found

--- src/pages/get-manifest.ts
+++ src/pages/get-manifest.ts
@@ -1,10 +1,12 @@
 import { readFile } from 'node:fs/promises'
+import type { APIRoute } from "astro";
 
+export const GET = async ({ locals }) => {
+  const { ASSETS } = locals
+  const res = await ASSETS.fetch('/manifest.br')
+//  const file = await readFile('public/manifest.br')
 
-export async function GET() {
-  const file = await readFile('public/manifest.br');
-
-  return new Response(file, {
+  return new Response(res, {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
       'Content-Encoding': 'br',        // <-- critical
