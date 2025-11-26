1b9da4de3ead525656970b308c740ea5bfe5d5c3
lorddesert <sacha.nocetto@hotmail.com>
2025-11-26 12:15:11 -0300

    hotfix manifest download

--- src/pages/get-manifest.ts
+++ src/pages/get-manifest.ts
@@ -2,11 +2,24 @@ import { readFile } from 'node:fs/promises'
 import type { APIRoute } from "astro";
 
 export const GET = async ({ locals }) => {
-  const { ASSETS } = locals
-  const res = await ASSETS.fetch('/manifest.br')
+//   const { ASSETS } = locals
+//   const res = await ASSETS.fetch('/manifest.br')
 //  const file = await readFile('public/manifest.br')
 
-  return new Response(res, {
+  const env = locals.runtime?.env;
+
+  if (env?.ASSETS) {
+    // ⛅ running on Cloudflare (or wrangler dev)
+    const resp = await env.ASSETS.fetch("/manifest.br");
+    return new Response(resp.body, { headers: resp.headers });
+  }
+
+  // 🖥️ local dev fallback
+  const file = await readFile("public/manifest.br");
+
+
+
+  return new Response(file, {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
       'Content-Encoding': 'br',        // <-- critical
