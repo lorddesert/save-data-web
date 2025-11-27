66ccd7fbcdf04e4f246e1e30bc3191eee8c26344
lorddesert <sacha.nocetto@hotmail.com>
2025-11-27 18:35:55 -0300

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
