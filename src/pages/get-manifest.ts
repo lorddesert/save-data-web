c420c87c480b677c263b502dd42a7dc7e1359cd8
lorddesert <sacha.nocetto@hotmail.com>
2025-11-27 19:57:03 -0300

    hotfix env locals runtime not found

--- src/pages/get-manifest.ts
+++ src/pages/get-manifest.ts
@@ -1,14 +1,13 @@
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ request, locals }) => {
-  const { env } = locals.runtime
-  const file = await env.ASSETS.fetch(new URL(request.url).origin + '/manifest.br')
+  const res = await fetch('http://localhost:4321/manifest.br')
 
-  if (!file) {
+  if (!res) {
     return new Response("Manifest not found", { status: 404 });
   }
 
-  return new Response(file, {
+  return new Response(res.body, {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
       'Content-Encoding': 'br',        // <-- critical
