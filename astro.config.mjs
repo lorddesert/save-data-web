8b45e7c44bffd8e13e2b5e28792bce169e949c8e
lorddesert <sacha.nocetto@hotmail.com>
2025-11-27 19:54:33 -0300

    feat change cloudflare workers to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vue from '@astrojs/vue';
-
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
--- src/pages/get-manifest.ts
+++ src/pages/get-manifest.ts
@@ -1,37 +1,8 @@
 import type { APIRoute } from 'astro';
-import { readFile } from 'node:fs/promises'
-
-function isCloudflareWorkers() {
-  return (
-    typeof navigator !== "undefined" &&
-    navigator.userAgent === "Cloudflare-Workers"
-  );
-}
 
 export const GET: APIRoute = async ({ request, locals }) => {
-  //   const { ASSETS } = locals
-  //   const res = await ASSETS.fetch('/manifest.br')
-  //  const file = await readFile('public/manifest.br')
-
-
-
-  if (isCloudflareWorkers()) {
-    const { env } = locals.runtime;
-    const url = new URL(request.url).origin + '/manifest.br'
-
-    const res = await env.ASSETS.fetch(url)
-
-    return new Response(res.body, {
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Content-Encoding': 'br',        // <-- critical
-        'Cache-Control': 'public, max-age=3600'
-      }
-    });
-  }
-
-  // 🖥️ local dev fallback
-  const file = await readFile("public/manifest.br");
+  const { env } = locals.runtime
+  const file = await env.ASSETS.fetch(new URL(request.url).origin + '/manifest.br')
 
   if (!file) {
     return new Response("Manifest not found", { status: 404 });
