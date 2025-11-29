d491de8ed829fe269c25b0dcbe03d4c699797565
lorddesert <sacha.nocetto@hotmail.com>
2025-11-29 02:15:46 -0300

    feat change host provider and add .vercel exclusion

--- src/pages/get-manifest.ts
+++ src/pages/get-manifest.ts
@@ -1,9 +1,11 @@
+export const prerender = false
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ request, locals }) => {
-  const res = await fetch('http://localhost:4321/manifest.br')
 
-  if (!res) {
+  const res = await fetch(`http://localhost:4321/manifest.br`)
+
+  if (!res.ok) {
     return new Response("Manifest not found", { status: 404 });
   }
 
