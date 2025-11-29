628cfc3e684d875591aedb2147795346a704d9de
lorddesert <sacha.nocetto@hotmail.com>
2025-11-29 17:07:14 -0300

    fix vercel doesn't build: fetch failed

--- src/pages/get-manifest.ts
+++ src/pages/get-manifest.ts
@@ -1,15 +1,27 @@
 export const prerender = false
 import type { APIRoute } from 'astro';
+import { promises as fs } from 'fs';
+import { readFile } from 'node:fs/promises';
+import path from 'path';
+
+async function getManifest() {
+  const publicDirectoryPath = path.join(process.cwd(), 'public');
+  const filePath = path.join(publicDirectoryPath, 'manifest.br');
+  const file = await readFile(filePath);
+
+  return file;
+}
+
 
 export const GET: APIRoute = async ({ request, locals }) => {
 
-  const res = await fetch(`http://localhost:4321/manifest.br`)
+  const manifest = await getManifest();
 
-  if (!res.ok) {
+  if (!manifest) {
     return new Response("Manifest not found", { status: 404 });
   }
 
-  return new Response(res.body, {
+  return new Response(manifest, {
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
       'Content-Encoding': 'br',        // <-- critical
